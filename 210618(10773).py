bf04968425201ef0f24566ec209c68d22c0d4e99
donghyun1500 <85468215+donghyun1500@users.noreply.github.com>
2021-06-18 23:17:20 +0900

    Update 210618(10773).py

--- 210618(10773).py
+++ 210618(10773).py
@@ -1,6 +1,7 @@
 #알고리즘 문제풀이 5일차!!!!!#
 
 https://www.acmicpc.net/problem/10773
+문제 링크 참조!!!
 
 
 n = int(input())           # n변수로 정수를 입력받는다.
@@ -13,15 +14,6 @@ for i in range(n):         #for문으로 num변수를 설정해서 그 다음 �
     else:                  #이 문제에서는 항상 pop을 보장한다고 했으므로 예외처리 nono!!!
         stack.append(num)   #받은 값이 0이 아닌경우 배열에 추가!!!
 print(sum(stack))
-
-
-
-
-
-
-
-
-
 
 
 #예제 입력 2를 예시로 들자면 n변수로 정수 10을 입력받아서 for문을 통해 10번 반복을 하고
